13cf78be48765f8f2636391943d4232021f7072e
Zoran Kikic <zoran@kikic.name>
2015-02-17 14:00:17 +0100

    minor change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require "api_constraint"
 
 Rails.application.routes.draw do
-  namespace :tracktor, defaults: { format: :json }, path: "/" do
+  namespace :tracktor, defaults: { format: "json" }, path: "/" do
     scope module: "v1", constraints: ApiConstraint.new(version: 1) do
       resources :locations, via: [:options, :post], only: :create,
           constraints: ApiSubDomainConstraint.new(Rails.configuration.valid_subdomains)
